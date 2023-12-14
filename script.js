a333c888219aa1a73d26ca4e27531a28b0661fd4
Maksym <bebeshkomaksym1997@gmail.com>
2023-12-14 20:22:32 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// function reverseArray(arr){
+//     const reversedArray = arr.reverse(arr);
+//     return reversedArray;
+// }
+// let arr = [1,2,3,4,5,6];
+// console.log(reverseArray(arr));
+//////////////////////////////////////////////////////////////////////////////////////////
+// function uniqueValues(arr1,arr2){
+//    const combinedArray = arr1.concat(arr2);
+//    const uniqueValuesArray = [...new Set(combinedArray)];
+//    return uniqueValuesArray;
+// }
+// const arr1 = [1,2,3,4,5];
+// const arr2 = [3,4,5,6,7];
+// console.log(uniqueValues(arr1,arr2));
+//////////////////////////////////////////////////////////////////////////////////////////
+// function calculateAverageGrade(students) {
+  
+//     const totalGrade = students.reduce((sum, student) => sum + student.grade, 0);
+
+//     const averageGrade = totalGrade / students.length;
+  
+//     return Math.round(averageGrade * 10) / 10;
+//   }
+  
+//   const students = [
+//     { name: "Alice", age: 20, grade: 4.5 },
+//     { name: "Bob", age: 21, grade: 3.9 },
+//     { name: "Charlie", age: 19, grade: 4.8 }
+//   ];
+  
+//   console.log(calculateAverageGrade(students));
+//////////////////////////////////////////////////////////////////////////////////////////
